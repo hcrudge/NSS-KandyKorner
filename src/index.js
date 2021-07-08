51333e05b76ca6d17e74d1d32bcd50ce3f375c07
hcrudge <hcrudge1@gmail.com>
2021-07-08 16:01:29 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router } from "react-router-dom"
+import { KandyKorner } from ".components/KandyKorner"
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
