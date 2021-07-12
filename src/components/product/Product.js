641b1ca7736729db28da6bc73ff0ce6a135a2d06
hcrudge <hcrudge1@gmail.com>
2021-07-12 15:10:11 -0500

    add product types display the long way

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,10 +1,10 @@
 import React from "react"
 import "./Product.css"
 
-export const ProductCard = ({product}) => (
+export const ProductCard = ({ product, productType}) => (
     <section className="product">
         <h3 className="product__name">{product.product}</h3>
-        <div className="product__type">{product.productType.type}</div>
+        <div className="product__type">{productType.type}</div>
         <div className="product__price">{product.price}</div>
     </section>
 )
--- src/components/product/ProductList.js
+++ src/components/product/ProductList.js
@@ -2,15 +2,18 @@ import React, { useContext, useEffect } from "react"
 import { ProductContext } from "./ProductProvider"
 import { ProductCard } from "./Product"
 import "./Product.css"
+import { ProductTypeContext } from "./ProductTypeProvider"
 
 export const ProductList = () => {
   // This state changes when `getProducts()` is invoked below
   const { products, getProducts } = useContext(ProductContext)
+  const { productTypes, getProductTypes } = useContext(ProductTypeContext)
 
   //useEffect - reach out to the world for something
   useEffect(() => {
     console.log("ProductList: useEffect - getProducts")
-    getProducts()
+    getProductTypes()
+    .then(getProducts)
 
   }, [])
 
@@ -20,7 +23,10 @@ export const ProductList = () => {
       {console.log("ProductList: Render", products)}
       {
         products.map(product => {
-          return <ProductCard key={product.id} product={product} />
+            const foundProductType = productTypes.find(productType => productType.id === product.productTypeId)
+            
+            console.log(foundProductType)
+          return <ProductCard key={product.id} product={product} productType={foundProductType} />
         })
       }
     </div>
